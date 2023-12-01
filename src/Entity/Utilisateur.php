221112641da293b94e800f23a65e0d23d64a8dde
vaal <lym.valentin@gmail.com>
2023-12-01 16:09:50 +0100

    admin-register

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -21,7 +21,7 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column]
-    private array $roles = [];
+    private array $roles = ["ROLE_ADMIN","ROLE_USER"];
 
     /**
      * @var string The hashed password
