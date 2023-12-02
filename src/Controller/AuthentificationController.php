38be6498c95f584ca8725f351ee1f9ab568efd6e
vaal <lym.valentin@gmail.com>
2023-12-02 23:45:39 +0100

    - register

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -12,9 +12,9 @@ class AuthentificationController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('login');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
